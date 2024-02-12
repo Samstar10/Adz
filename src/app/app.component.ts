e01c0b02bd830c6820891b21fef4d03896f13f6b
Samuel Muli <samuelmuli95@gmail.com>
2024-02-12 14:43:22 +0300

    Create Sign up step one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignUpStepOneComponent } from './sign-up-step-one/sign-up-step-one.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     LoginComponent, 
-    RouterOutlet
+    RouterOutlet,
+    SignUpStepOneComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/sign-up-step-one/sign-up-step-one.component.ts
+++ src/app/sign-up-step-one/sign-up-step-one.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import intlTelInput from 'intl-tel-input';
 
 @Component({
   selector: 'app-sign-up-step-one',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './sign-up-step-one.component.html',
   styleUrl: './sign-up-step-one.component.css'
 })
-export class SignUpStepOneComponent {
+export class SignUpStepOneComponent implements AfterViewInit {
+  @ViewChild('phoneInput')
+  phoneInput!: ElementRef;
+
+  private iti: any
+
+  ngAfterViewInit(): void {
+    this.iti = intlTelInput(this.phoneInput.nativeElement, {
+      initialCountry: 'ke',
+      separateDialCode: true,
+      utilsScript: "node_modules/intl-tel-input/build/js/intlTelInput.js"
+    })
+  }
 
 }

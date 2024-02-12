24c533ef36505815de0bc109aa411160b65927e3
Samuel Muli <samuelmuli95@gmail.com>
2024-02-12 10:49:28 +0300

    Set up intl-tel package

---
+++ src/app/intl-tel-input.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appIntlTelInput]',
+  standalone: true
+})
+export class IntlTelInputDirective {
+
+  constructor() { }
+
+}

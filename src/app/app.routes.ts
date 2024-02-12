83118e5e7eab10032fb60c5612ac9044065c2be2
Samuel Muli <samuelmuli95@gmail.com>
2024-02-12 15:33:54 +0300

    Create sign up step two component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpStepOneComponent } from './sign-up-step-one/sign-up-step-one.component';
+import { SignUpStepTwoComponent } from './sign-up-step-two/sign-up-step-two.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'sign-up-one',
         component: SignUpStepOneComponent
+    },
+    {
+        path: 'sign-up-two',
+        component: SignUpStepTwoComponent
     }
 ];

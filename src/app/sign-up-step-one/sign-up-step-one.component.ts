8da88250d4925f8061a6654e28478609d9a05993
Samuel Muli <samuelmuli95@gmail.com>
2024-02-12 10:44:50 +0300

    Initialize sign-up-step-one component

---
+++ src/app/sign-up-step-one/sign-up-step-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-step-one',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up-step-one.component.html',
+  styleUrl: './sign-up-step-one.component.css'
+})
+export class SignUpStepOneComponent {
+
+}

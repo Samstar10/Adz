cc2d607e773e4525145857892299eec20fa3da77
Samuel Muli <samuelmuli95@gmail.com>
2024-02-12 14:45:01 +0300

    Initialize sign up step two component

---
+++ src/app/sign-up-step-two/sign-up-step-two.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-step-two',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up-step-two.component.html',
+  styleUrl: './sign-up-step-two.component.css'
+})
+export class SignUpStepTwoComponent {
+
+}

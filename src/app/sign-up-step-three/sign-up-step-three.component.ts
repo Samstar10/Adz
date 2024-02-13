c867386d9886de31cec277c768a0f02283ce6fb5
Samuel Muli <samuelmuli95@gmail.com>
2024-02-13 11:37:38 +0300

    Initialize sign up step three component

---
+++ src/app/sign-up-step-three/sign-up-step-three.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-step-three',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up-step-three.component.html',
+  styleUrl: './sign-up-step-three.component.css'
+})
+export class SignUpStepThreeComponent {
+
+}

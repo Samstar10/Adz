9f3d6f37fa9ae023bf563fcf6a7facbd25a063e4
Samuel Muli <samuelmuli95@gmail.com>
2024-02-15 17:39:54 +0300

    Add button navigation to sign up step three

--- src/app/sign-up-step-three/sign-up-step-three.component.ts
+++ src/app/sign-up-step-three/sign-up-step-three.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-step-three',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './sign-up-step-three.component.html',
   styleUrl: './sign-up-step-three.component.css'
 })
 export class SignUpStepThreeComponent {
 
+  constructor(private router: Router){}
+
+  redirectTo(){
+    this.router.navigate(['/sign-up-four']);
+  }
 }

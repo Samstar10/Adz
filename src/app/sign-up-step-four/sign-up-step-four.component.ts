93da412feee70cbcdf931064809dd1c56c91b805
Samuel Muli <samuelmuli95@gmail.com>
2024-02-15 19:49:39 +0300

    Set up sign in reroute

--- src/app/sign-up-step-four/sign-up-step-four.component.ts
+++ src/app/sign-up-step-four/sign-up-step-four.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-step-four',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './sign-up-step-four.component.html',
   styleUrl: './sign-up-step-four.component.css'
 })

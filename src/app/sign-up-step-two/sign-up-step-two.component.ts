efa7e5352e28094040f0979ef98df8013554967d
Samuel Muli <samuelmuli95@gmail.com>
2024-02-15 17:29:04 +0300

    Add button navigation to sign up component two

--- src/app/sign-up-step-two/sign-up-step-two.component.ts
+++ src/app/sign-up-step-two/sign-up-step-two.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-step-two',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './sign-up-step-two.component.html',
   styleUrl: './sign-up-step-two.component.css'
 })
 export class SignUpStepTwoComponent {
 
+  constructor(private router: Router){}
+
+  redirectTo(){
+    this.router.navigate(['/sign-up-three']);
+  }
 }

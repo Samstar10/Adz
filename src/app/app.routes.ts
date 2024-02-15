d4793b3d3f8d0fd79ff10b304b8de1916d3b20d6
Samuel Muli <samuelmuli95@gmail.com>
2024-02-15 17:54:01 +0300

    Set up create account redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
         component: LoginComponent
     },
     {

dd4fb377706e9a01527c1746f0845f45a0455d46
Samuel Muli <samuelmuli95@gmail.com>
2024-02-16 11:48:15 +0300

    Set up Sign up service

--- src/app/sign-up-service.service.ts
+++ src/app/sign-up-service.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { SignUpData } from './sign-up-data';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignUpServiceService {
+  private signUpdata: SignUpData = {
+    companyName: '',
+    companyEmail: '',
+    adminUsername: '',
+    phoneNumber: '',
+    password: '',
+    confirmPassword: '',
+    companySize: '',
+    currency: '',
+    certOfRegistration: '',
+    companyByLaws: '',
+    companyLogo: '',
+    companyThemeColor: ''
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  updateData(data: Partial<SignUpData>) {
+    this.signUpdata = { ...this.signUpdata, ...data };
+  }
+
+  submitData(): Observable<any> {
+    return this.http.post('http://localhost:3000/users', this.signUpdata);
+  }
 }

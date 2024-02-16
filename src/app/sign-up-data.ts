d72ebbfdd48c773164cfcecbd073979ae6d85a5a
Samuel Muli <samuelmuli95@gmail.com>
2024-02-16 11:42:08 +0300

    Set Up Sign Up interface

--- src/app/sign-up-data.ts
+++ src/app/sign-up-data.ts
@@ -1,2 +1,14 @@
 export interface SignUpData {
+    companyName?: string;
+    companyEmail: string;
+    adminUsername?: string;
+    phoneNumber?: string;
+    password: string;
+    confirmPassword?: string;
+    companySize?: string;
+    currency?: string;
+    certOfRegistration?: string;
+    companyByLaws?: string;
+    companyLogo?: string;
+    companyThemeColor?: string;
 }

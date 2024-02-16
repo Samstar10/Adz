e3d41034d9cf2d8b6e165eff7b377b57a647294f
Samuel Muli <samuelmuli95@gmail.com>
2024-02-16 11:57:03 +0300

    Implement sign up functionality in my sign up step one component

--- src/app/sign-up-step-one/sign-up-step-one.component.ts
+++ src/app/sign-up-step-one/sign-up-step-one.component.ts
@@ -1,20 +1,36 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import { SignUpServiceService } from '../sign-up-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up-step-one',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    HttpClientModule
   ],
+  providers: [SignUpServiceService],
   templateUrl: './sign-up-step-one.component.html',
   styleUrl: './sign-up-step-one.component.css'
 })
 export class SignUpStepOneComponent {
+  companyName: string = '';
+  companyEmail: string = '';
+  adminUsername: string = '';
+  phoneNumber: string = '';
+  password: string = '';
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private signUpService: SignUpServiceService){}
   
   redirectTo(){
+    this.signUpService.updateData({
+      companyName: this.companyName,
+      companyEmail: this.companyEmail,
+      adminUsername: this.adminUsername,
+      phoneNumber: this.phoneNumber,
+      password: this.password
+    })
     this.router.navigate(['/sign-up-two']);
   }
 }

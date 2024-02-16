d03f3afe7bf4411132cc818c70f6c0158a66e134
Samuel Muli <samuelmuli95@gmail.com>
2024-02-16 11:05:36 +0300

    Set up log in service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<boolean> {  
+    return this.http.get<any[]>('http://localhost:3000/users').pipe(
+      map(users => {
+        const user = users.find(u => u.email === username && u.password === password);
+        if (user) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
+  }
 }

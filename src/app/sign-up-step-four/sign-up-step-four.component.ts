8e173edd1ba12485d02a706ecc5e688b183e61e5
Samuel Muli <samuelmuli95@gmail.com>
2024-02-16 12:52:50 +0300

    Finish implementing sign up functionality

--- src/app/sign-up-step-four/sign-up-step-four.component.ts
+++ src/app/sign-up-step-four/sign-up-step-four.component.ts
@@ -1,15 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { SignUpServiceService } from '../sign-up-service.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up-step-four',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    FormsModule,
+    HttpClientModule
   ],
+  providers: [SignUpServiceService],
   templateUrl: './sign-up-step-four.component.html',
   styleUrl: './sign-up-step-four.component.css'
 })
 export class SignUpStepFourComponent {
+
+  companyLogo: string = '';
+  companyThemeColor: string = '';
+
+  constructor(private router: Router, private signUpService: SignUpServiceService){}
+
+  completeSignUp(){
+    if(this.companyLogo === '' || this.companyThemeColor === ''){
+      alert('Please fill all fields');
+    }else{
+      this.signUpService.submitData().subscribe({
+        next: (response) => {
+          console.log('Sign Up successful',response);
+          this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          console.log('Sign Up failed',error);
+        }
+      })
+    }
+  }
 
 }

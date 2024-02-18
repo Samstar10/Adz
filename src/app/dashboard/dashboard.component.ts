5d8b56404688166827376085f51da648d88e0a6d
Samuel Muli <samuelmuli95@gmail.com>
2024-02-18 20:12:48 +0300

    Create viewers graph

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgChartsModule } from 'ng2-charts';
 import { GraphsDataService } from '../graphs-data.service';
-import { ChartConfiguration, ChartType, Chart, registerables } from 'chart.js';
+import { ChartConfiguration, ChartType, ChartOptions, Chart, registerables } from 'chart.js';
 import { map } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -12,52 +12,67 @@ Chart.register(...registerables);
   standalone: true,
   imports: [
     NgChartsModule,
-    HttpClientModule
+    HttpClientModule,
+    // BaseChartDirective
   ],
   providers: [GraphsDataService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  @ViewChild('chartCanvas') chartCanvas!: ElementRef;
 
-  // public chartType: ChartType = 'line';
-  // public chartOptions: ChartConfiguration['options'] = {
-  //   responsive: true,
-  //   scales: {
-  //     y: {
-  //       beginAtZero: true
-  //     }
-  //   }
-  // }
-  // public chartData: ChartConfiguration['data']= {
-  //   datasets: [],
-  //   labels: []
-  // };
+  constructor(private graphsDataService: GraphsDataService){}
 
-  // @ViewChild('chartCanvas') chartCanvas!: ElementRef<HTMLCanvasElement>
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'Sep 27',
+      'Sep 28',
+      'Sep 29',
+      'Sep 30',
+      'Oct 1',
+      'Oct 2',
+      'Oct 3',
+      'Oct 4'
+    ],
+    datasets: [
+      {
+        data: [600, 400, 200, 400, 600, 200, 200, 500],
+        label: 'Viewers',
+        fill: true,
+        tension: 0.4,
+        borderColor: '#FD6A02',
+        backgroundColor: 'rgba(253,106,2,0.5)'
+      }
+    ]
+  };
 
-  // constructor(private graphsDataService: GraphsDataService){}
+  public lineChartOptions: ChartConfiguration<'line'>['options'] = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 200
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  };
+  public lineChartLegend = false;
 
-  // ngAfterViewInit(){
-  //   this.graphsDataService.getData().pipe(
-  //     map((data) => data.map(d => ({ x: d.label, y: d.value })))
-  //   ).subscribe(mappedData => {
-  //     const ctx = this.chartCanvas.nativeElement.getContext('2d');
-  //     const gradient = ctx!.createLinearGradient(0, 0, 0, 400);
-  //     gradient.addColorStop(0, 'rgba(255, 99, 132, 0.2)');
-  //     gradient.addColorStop(1, 'rgba(255, 99, 132, 0)');
+  ngAfterViewInit(): void {
+    const chartContext = this.chartCanvas.nativeElement.getContext('2d');
+    const gradient = chartContext.createLinearGradient(0, 0, 0, this.chartCanvas.nativeElement.offsetHeight);
+    gradient.addColorStop(0, 'rgba(253,106,2,1)');
+    gradient.addColorStop(1, 'rgba(253,106,2,0.2)');
+    this.lineChartData.datasets[0].backgroundColor = gradient;
 
-  //     this.chartData = {
-  //       datasets: [{
-  //         data: mappedData,
-  //         backgroundColor: gradient,
-  //         borderColor: 'rgb(255, 99, 132)',
-  //         pointBackgroundColor: 'rgb(255, 99, 132)',
-  //         fill: true
-  //       }],
-  //       labels: mappedData.map(d => d.x)
-  //     }
-  //   })
-  // }
+    this.chartCanvas.nativeElement.update()
+  }
 
 }

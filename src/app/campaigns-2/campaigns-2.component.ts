f2377a29cec3e25e698b040d9432e632dc83d1b5
Samuel Muli <samuelmuli95@gmail.com>
2024-02-20 17:39:20 +0300

    Initialize first step new campaign form

--- src/app/campaigns-2/campaigns-2.component.ts
+++ src/app/campaigns-2/campaigns-2.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-campaigns-2',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './campaigns-2.component.html',
   styleUrl: './campaigns-2.component.css'
 })
 export class Campaigns2Component {
+
+  currentYear: number = new Date().getFullYear();
 
 }

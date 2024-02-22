6758769971cecb6c645db06641df5b9ad594cbb7
Samuel Muli <samuelmuli95@gmail.com>
2024-02-22 14:32:30 +0300

    Initialize campaigns three component

---
+++ src/app/campaigns-three/campaigns-three.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-three',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-three.component.html',
+  styleUrl: './campaigns-three.component.css'
+})
+export class CampaignsThreeComponent {
+
+}

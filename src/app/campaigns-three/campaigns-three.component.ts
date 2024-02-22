9154d69354f0d4a86496e131e792e58175306b3b
Samuel Muli <samuelmuli95@gmail.com>
2024-02-22 14:41:24 +0300

    Set up first step campaign component boiler plate

--- src/app/campaigns-three/campaigns-three.component.ts
+++ src/app/campaigns-three/campaigns-three.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-campaigns-three',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './campaigns-three.component.html',
   styleUrl: './campaigns-three.component.css'
 })
 export class CampaignsThreeComponent {
+  currentYear: number = new Date().getFullYear();
+  isSideBarVisible: boolean = false;
 
+  constructor(private router: Router){}
+
+  redirectTo(){
+    this.router.navigate(['/campaigns-4']);
+  }
+
+  toggleSidebar(): void {
+    this.isSideBarVisible = !this.isSideBarVisible;
+  }
 }

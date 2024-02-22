ee9e1ed37fc4e94f0cec9c66d74b0c87794153db
Samuel Muli <samuelmuli95@gmail.com>
2024-02-22 16:57:30 +0300

    Style updates

--- src/app/campaigns-four/campaigns-four.component.ts
+++ src/app/campaigns-four/campaigns-four.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-campaigns-four',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './campaigns-four.component.html',
   styleUrl: './campaigns-four.component.css'
 })
 export class CampaignsFourComponent {
+  isSideBarVisible: boolean = false;
+
+  currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router){}
+
+  toggleSidebar(): void {
+    this.isSideBarVisible = !this.isSideBarVisible;
+  }
+
+  redirectTo(){
+    this.router.navigate(['/campaigns-3']);
+  }
+
+  redirectBack(){
+    this.router.navigate(['/campaigns-2']);
+  }
 
 }

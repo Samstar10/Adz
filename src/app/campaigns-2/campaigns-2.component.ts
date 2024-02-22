a3f5145896666effd8baf9e77466e37ff6df4356
Samuel Muli <samuelmuli95@gmail.com>
2024-02-22 12:38:00 +0300

    Create campaign step 0 form

--- src/app/campaigns-2/campaigns-2.component.ts
+++ src/app/campaigns-2/campaigns-2.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-campaigns-2',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './campaigns-2.component.html',
   styleUrl: './campaigns-2.component.css'
 })
 export class Campaigns2Component {
 
+  isSideBarVisible: boolean = false;
+
   currentYear: number = new Date().getFullYear();
+
+  toggleSidebar(): void {
+    this.isSideBarVisible = !this.isSideBarVisible;
+  }
 
 }

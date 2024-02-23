4809b6329bcde708201af070645ae21a31ce505a
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 12:31:22 +0300

    Generate seventh campaign component

---
+++ src/app/campaigns-seven/campaigns-seven.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-seven',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-seven.component.html',
+  styleUrl: './campaigns-seven.component.css'
+})
+export class CampaignsSevenComponent {
+
+}

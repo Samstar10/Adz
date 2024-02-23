c77f6b6c3b931fde03b9c19e15520071ee3814bd
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 17:04:14 +0300

    Generate 8th campaigns component

---
+++ src/app/campaigns-eight/campaigns-eight.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-eight',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-eight.component.html',
+  styleUrl: './campaigns-eight.component.css'
+})
+export class CampaignsEightComponent {
+
+}

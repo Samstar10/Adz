198d27b459ced1d86781c13e210ac62fbebbfe5e
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 12:48:35 +0300

    Create seventh campaigns component

--- src/app/campaigns-seven/campaigns-seven.component.ts
+++ src/app/campaigns-seven/campaigns-seven.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-campaigns-seven',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './campaigns-seven.component.html',
   styleUrl: './campaigns-seven.component.css'
 })
 export class CampaignsSevenComponent {
+  isSideBarVisible: boolean = false;
+
+  currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router){}
+
+  toggleSidebar(): void {
+    this.isSideBarVisible = !this.isSideBarVisible;
+  }
+
+  redirectTo(){
+    this.router.navigate(['/campaigns-7']);
+  }
+
+  redirectBack(){
+    this.router.navigate(['/campaigns-5']);
+  }
+
 
 }

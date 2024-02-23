400baa2bc436504f759b180be934133de3defc18
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 12:12:55 +0300

    generate geolocation component

---
+++ src/app/campaigns-six/campaigns-six.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-six',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-six.component.html',
+  styleUrl: './campaigns-six.component.css'
+})
+export class CampaignsSixComponent {
+
+}

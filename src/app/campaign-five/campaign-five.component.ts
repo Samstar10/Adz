f856dfcf677dad4a0cc102b700ce506d78e9e2d4
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 11:32:23 +0300

    Set up step three boiler plate code

--- src/app/campaign-five/campaign-five.component.ts
+++ src/app/campaign-five/campaign-five.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-campaign-five',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './campaign-five.component.html',
   styleUrl: './campaign-five.component.css'
 })
 export class CampaignFiveComponent {
+  isSideBarVisible: boolean = false;
+
+  currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router){}
+
+  toggleSidebar(): void {
+    this.isSideBarVisible = !this.isSideBarVisible;
+  }
+
+  redirectTo(){
+    this.router.navigate(['/campaigns-5']);
+  }
+
+  redirectBack(){
+    this.router.navigate(['/campaigns-3']);
+  }
 
 }

ba428ed7b4078dc542a3278cf4186d9a0bca2396
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 12:27:48 +0300

    Create template for geolocation component

--- src/app/campaigns-six/campaigns-six.component.ts
+++ src/app/campaigns-six/campaigns-six.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-campaigns-six',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './campaigns-six.component.html',
   styleUrl: './campaigns-six.component.css'
 })
 export class CampaignsSixComponent {
+  isSideBarVisible: boolean = false;
+
+  currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router){}
+
+  toggleSidebar(): void {
+    this.isSideBarVisible = !this.isSideBarVisible;
+  }
+
+  redirectTo(){
+    this.router.navigate(['/campaigns-6']);
+  }
+
+  redirectBack(){
+    this.router.navigate(['/campaigns-4']);
+  }
 
 }

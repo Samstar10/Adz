121be7331edba46d24cad03a36c35abfdb29de75
Samuel Muli <samuelmuli95@gmail.com>
2024-02-23 16:50:24 +0300

    Integrate google maps

--- src/app/campaigns-six/campaigns-six.component.ts
+++ src/app/campaigns-six/campaigns-six.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
+
+declare let google: any;
 
 @Component({
   selector: 'app-campaigns-six',
@@ -14,11 +16,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './campaigns-six.component.css'
 })
 export class CampaignsSixComponent {
+  @ViewChild('map') mapElement!: ElementRef;
+  @ViewChild('location') locationInput!: ElementRef;
+  map: any
+  geocoder: any
+  autocomplete: any
   isSideBarVisible: boolean = false;
 
   currentYear: number = new Date().getFullYear();
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private ngZone: NgZone){}
 
   toggleSidebar(): void {
     this.isSideBarVisible = !this.isSideBarVisible;
@@ -32,4 +39,44 @@ export class CampaignsSixComponent {
     this.router.navigate(['/campaigns-5']);
   }
 
+  ngOnInit(): void {
+    this.initMap()
+  }
+
+  initMap(): void {
+    const center = new google.maps.LatLng(35.2271, -80.8431);
+    const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+      zoom: 8,
+      center: center,
+    });
+    new google.maps.Marker({
+      position: center,
+      map: map,
+    });
+  }
+
+  initAutoComplete(): void {
+    this.autocomplete = new google.maps.places.Autocomplete(this.locationInput.nativeElement, {
+      types: ['geocode'],
+    })
+    this.autocomplete.addListener('place_changed', () => {
+      this.ngZone.run(() => {
+        const place = this.autocomplete.getPlace()
+        if (!place.geometry) {
+          return
+        }
+        if (place.geometry.viewport){
+          this.map.fitBounds(place.geometry.viewport)
+        } else {
+          this.map.setCenter(place.geometry.location)
+          this.map.setZoom(17)
+        }
+        // Place a marker on the selected location
+        new google.maps.Marker({
+          map: this.map,
+          position: place.geometry.location
+        })
+      })
+    })
+  }
 }

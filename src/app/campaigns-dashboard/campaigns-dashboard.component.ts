bb7104b27a3b3ed49117ef6976cd6c216c3a79cd
Samuel Muli <samuelmuli95@gmail.com>
2024-02-26 09:32:00 +0300

    generate campaign dashboard component

---
+++ src/app/campaigns-dashboard/campaigns-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-dashboard.component.html',
+  styleUrl: './campaigns-dashboard.component.css'
+})
+export class CampaignsDashboardComponent {
+
+}

31fb690205e2eb7e095fa51c6e15449580901430
Samuel Muli <samuelmuli95@gmail.com>
2024-02-27 21:44:28 +0300

    generate transactions component

---
+++ src/app/transactions-home/transactions-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './transactions-home.component.html',
+  styleUrl: './transactions-home.component.css'
+})
+export class TransactionsHomeComponent {
+
+}
